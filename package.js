b9b1ffadb42b8bbfade08aaea10e4fbe890d21dd
Matt Crider <mattcrider@gmail.com>
2013-04-14 17:13:11 -1000

    Added jquery.hotkeys

--- package.js
+++ package.js
@@ -5,6 +5,7 @@ Package.describe({
 Package.on_use(function (api) {
     api.use('bootstrap', 'client');
     api.add_files([
+        'lib/jquery.hotkeys.js',
         'lib/bootstrap-wysiwyg.js'
     ], 'client');
 });
